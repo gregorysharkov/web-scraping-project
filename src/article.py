fca93cdd9991e34b3df3796da3c203da8e79d41a
Grigory Sharkov <gregorysharkov@gmail.com>
2023-08-21 07:02:37 +0800

    fix: file not found error when saving article

--- src/article.py
+++ src/article.py
@@ -13,9 +13,14 @@ class Article:
 
     def save(self, output_path: Path) -> None:
         '''saves given article in the given path'''
+        # make sure that the path exists
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        # clean article name
         clean_file_name = re.sub(r'[^a-zA-Z0-9]', '_', self.title)
         clean_file_name = re.sub('_{2,}', '_', clean_file_name)
 
+        # save
         file_name = output_path / f'{self.site} - {clean_file_name}.txt'
         with open(file_name, 'w', encoding='utf-8') as file:
             file.write(self.text)
